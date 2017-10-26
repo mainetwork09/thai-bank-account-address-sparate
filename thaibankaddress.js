0bab1a8317bca925a72030bdcf34f80f4f339841
Sirachon <maiphp@gmail.com>
2017-10-26 18:10:28 +0700

    [*] change function style to string prototype

--- thaibankaddress.js
+++ thaibankaddress.js
@@ -1,7 +1,11 @@
 /*
-* Javascript
-*/
+ * Javascript
+ */
 
-function thaiBankAddressFormat(address){
-    return ( address.length == 10 ) ? address.substring(0, 3) + '-' + address.substring(3, 4) + '-' + address.substring(4, 9) + '-' + address.substring(9, 10):address;
+String.prototype.thaiBankAddressFormat = function() {
+    return (this.length == 10) ? this.substring(0, 3) + '-' + this.substring(3, 4) + '-' + this.substring(4, 9) + '-' + this.substring(9, 10) : this;
 }
+
+/** Usage */
+var bankAddress = "1234567891";
+console.log(bankAddress.thaiBankAddressFormat())
